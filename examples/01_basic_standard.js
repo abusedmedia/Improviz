7c8a184a2e5987ea152880e70d5ed59ac617ac9d
abusedmedia <ff@todo.to.it>
2015-07-27 18:47:29 +0200

    added new interface with socket, basic example

--- examples/01_basic_standard.js
+++ examples/01_basic_standard.js
@@ -11,8 +11,13 @@ require('./../lib/Improviz.js');
 
 // we are going to use as interface the console.log
 var c = new Console();
+var m = new Midi(1);
+var s = new Socket();
 
-var p = new Player(c);
+var p = new Player();
+p.addInterface(c)
+p.addInterface(m)
+p.addInterface(s)
 p.play();
 
 var t = new Track(0);
