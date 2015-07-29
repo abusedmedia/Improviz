f970d0bed9113ae3c407e262b2afcaeae71f6510
abusedmedia <ff@todo.to.it>
2015-07-29 19:27:15 +0200

    some mods on lib based on tools needs

--- examples/02_basic_repl.js
+++ examples/02_basic_repl.js
@@ -1,5 +1,4 @@
-// simple run: node <name.js>
-// dev run: nodemon <name.js> --watch <path folder>
+//  run: node <name.js>
 
 /*
 Simple REPL example
@@ -10,19 +9,27 @@ No needs to stop the runner
 var myrepl = require("repl").start({useGlobal:true});
 myrepl.context = require('./../lib/Improviz.js');
 
-/*
-// REPL example, write this in console, line by line
-var m = new Midi(1);
-var p = new Player(m);
 
-var t = new Track();
-p.add(t);
+
+/*
+You can access the 'P' (since it's global) object from the console
+The code below will be the starting point
+You can modify during run time from the node.js console though
+*/
+S = new WebSocket();
+
+P = new Player();
+P.addInterface(new Midi(1))
+P.addInterface(S)
+P.play();
+
+var t = new Track(0);
+P.add(t);
 
 var s = new Sequence();
 t.add(s);
 
-s.add( new Note(60, 100, 127))
+var n = new Note( 60, 100, 127);
+s.add(n);
 
-p.play()
-*/
 
